58599a9f494beed475045af7e09d777987946ff4
Ward Peeters <ward@coding-tech.com>
2019-07-03 19:43:13 +0200

    fix: override loader & router to not redirect

---
+++ src/gatsby-browser.js
@@ -0,0 +1,25 @@
+
+// We override loadPage & loadPagesync to fix canonical redirects
+exports.onClientEntry = () => {
+  if (window.pagePath !== location.pathname && window.pagePath !== location.pathname + '/') {
+    const originalLoadPageSync = window.___loader.loadPageSync;
+    const originalLoadPage = window.___loader.loadPage;
+
+    window.___loader.loadPageSync = path => {
+      // if the path is the same as our current page we know it's not a prefetch
+      if (path === window.location.pathname) {
+        return originalLoadPageSync(window.pagePath);
+      }
+
+      return originalLoadPageSync(path);
+    }
+    window.___loader.loadPage = path => {
+      // if the path is the same as our current page we know it's not a prefetch
+      if (path === window.location.pathname) {
+        return originalLoadPage(window.pagePath);
+      }
+
+      return originalLoadPage(path);
+    }
+  }
+}
