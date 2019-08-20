48dee0a98fac5eb7d4474256f35f718e7dae5064
Xavier Ivars-Ribes <xivarsribes@vistaprint.com>
2019-08-20 16:17:12 +0200

    Include appropriate version of reach router
    Fixes https://github.com/wardpeet/gatsby-plugin-static-site/issues/5

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,10 +1,30 @@
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      alias: {
-        "@reach/router$": require.resolve(`./router`),
-        "@reach/router-original$": require.resolve(`@reach/router`)
+const path = require(`path`);
+
+exports.onCreateWebpackConfig = ({ actions, stage, getConfig }) => {
+  const target = stage === `build-html` || stage === `develop-html` ? `node` : `web`;
+
+  if (target === `web`) {
+    // force to use es modules when importing internals of @reach.router
+    // for browser bundles
+
+    delete getConfig().resolve.alias['@reach/router'];
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          "@reach/router/lib": path.join(path.dirname(require.resolve("@reach/router/package.json")), "es", "lib"),
+          "@reach/router$": require.resolve("./router"),
+          "@reach/router-original$": require.resolve("@reach/router/es")
+        }
       }
-    }
-  });
+    });
+  } else {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          "@reach/router$": require.resolve(`./router`),
+          "@reach/router-original$": require.resolve(`@reach/router`)
+        }
+      }
+    });
+  }
 };
