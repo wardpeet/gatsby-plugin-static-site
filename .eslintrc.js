5157e65630224a719e01c860865971c7ed32583d
Ward Peeters <ward@coding-tech.com>
2019-11-16 17:56:16 +0100

    chore: format all the things

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,39 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    }
   },
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
   },
-  overrides: [{
-    files: ["./src/gatsby-browser.js", "./src/router/*"],
-    env: {
-      browser: true,
-    }
-  }]
+  overrides: [
+    {
+      files: ["./src/gatsby-browser.js", "./src/loader/*"],
+      env: {
+        browser: true,
+      }
+    },
+    {
+      files: ["**/cypress/**/*"],
+      "env": {
+        "cypress/globals": true
+      },
+      plugins: ["cypress"]
+    },
+    {
+      files: ["e2e-tests/*/src/**/*"],
+      extends: ["plugin:react/recommended"],
+      rules: {
+        "react/prop-types": 0
+      },
+      "settings": {
+        "react": {
+          "version": "16.9.0"
+        },
+      },
+    },
+  ],
 };
