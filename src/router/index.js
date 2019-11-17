d8e8214dc3e4dd1e816946728725bb45373dce42
Ward Peeters <ward@coding-tech.com>
2019-11-17 12:06:43 +0100

    fix: reach-router navigate export (#16)

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-export * from '@reach/router-original';
-
-export const navigate = (url, options) => {
-  // when we do not replace the current url or the replacement is just adding a slash we navigate
-  // this fixes canonical redirects
-  if (!options.replace || window.location.href + '/' === url) {
-    window.location = url;
-  }
-};
