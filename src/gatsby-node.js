b46344df90969b92aade2c0076cdd3bbca3affcc
Ward Peeters <ward@coding-tech.com>
2019-11-17 20:06:34 +0100

    fix: disable loader override in develop (#18)

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,6 +1,11 @@
 const GatsbyStaticSitePlugin = require('./GatsbyStaticSitePlugin');
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  // don't add plugin 
+  if (stage !== 'build-javascript') {
+    return;
+  }
+
   // add webpack plugin to gatsby's webpack config
   actions.setWebpackConfig({
     plugins: [new GatsbyStaticSitePlugin()],
