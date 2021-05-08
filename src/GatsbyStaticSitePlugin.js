7e11682da9478105b540270a828538688fed070b
Ward Peeters <ward@coding-tech.com>
2021-05-09 00:45:51 +0200

    check both @reach & @gatsbyjs router

--- src/GatsbyStaticSitePlugin.js
+++ src/GatsbyStaticSitePlugin.js
@@ -18,7 +18,11 @@ module.exports = class GatsbyStaticSitePlugin {
             parser.hooks.importSpecifier.tap(
               pluginName,
               (statement, source, id, name) => {
-                if (source === '@gatsbyjs/reach-router' && id === 'navigate') {
+                if (
+                  (source === '@reach/router' ||
+                    source === '@gatsbyjs/reach-router') &&
+                  id === 'navigate'
+                ) {
                   // remove navigate from import
                   parser.scope.definitions.delete(name);
 
